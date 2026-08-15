b9d10aeaadbdecef7b90df4a506a9f4c2f43d274
Arthur <arthuroaraujo@gmail.com>
2026-08-15 17:53:42 -0300

    chore: setup frontend with vue and vuetify

---
+++ frontend/src/plugins/vuetify.ts
@@ -0,0 +1,20 @@
+import 'vuetify/styles';
+import '@mdi/font/css/materialdesignicons.css';
+
+import { createVuetify } from 'vuetify';
+
+export default createVuetify({
+  theme: {
+    defaultTheme: 'dark',
+
+    themes: {
+      dark: {
+        colors: {
+          background: '#141414',
+          surface: '#181818',
+          primary: '#E50914',
+        },
+      },
+    },
+  },
+});
